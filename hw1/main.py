b5ba2b5a432170d02d20c610e777cd0ceb2f0f78
gajagajago <gajagajago@naver.com>
2022-04-10 19:19:41 +0900

    insertion sort err

--- hw1/main.py
+++ hw1/main.py
@@ -1,6 +1,7 @@
 import time
 
 def insertion_sort(li):
+
     '''
     Sort list in ascending order
     :param li: list to be sorted
@@ -16,7 +17,8 @@ def insertion_sort(li):
 
         li[j+1] = x
 
-def partition(li, p ,r, pi):
+
+def partition(li, p, r, pi):
     '''
     Partition list based on pivot element
     :param li: partition target list
@@ -44,7 +46,6 @@ def random_select(li, p, r, i):
     :param p: start index of li
     :param r: end index of li
     :param i: i-th smallest
-    :return el: answer
     '''
     if p == r:
         return li[p]
@@ -59,8 +60,6 @@ def random_select(li, p, r, i):
     else:
         return li[q]
 
-    return el
-
 def deterministic_select(li, p, r, i):
     '''
     Finds i-th smallest element of li in worst O(n) time
@@ -68,10 +67,51 @@ def deterministic_select(li, p, r, i):
     :param p: start index of li
     :param r: end index of li
     :param i: i-th smallest
-    :return el: answer
     '''
+    size = 5
+    n = r-p+1
 
-    return el
+    if n <= size:
+        insertion_sort(li)
+        return li[i]
+
+    grp_n, lo = divmod(n, size)
+    if lo != 0:
+        grp_n += 1
+    print("Grps[%d], Leftover: %d\n" % (grp_n, lo))
+
+    m = []
+    for j in range(0, grp_n-1):
+        insertion_sort(li[size*j : size*(j+1)])
+        print(li[size*j : size*(j+1)])
+        median = li[size*j + size//2]
+        m.append(median)
+        print("j[%d], median[%d]" % (j, median))
+
+    if lo != 0:
+        insertion_sort(li[size*(grp_n-1) : ])
+        median = li[size*(grp_n-1) + lo//2]
+    else:
+        insertion_sort(li[size*(grp_n-1) : ])
+        median = li[size*(grp_n-1) + size//2]
+    m.append(median)
+    print("j[%d], median[%d]" % (grp_n-1, median))
+
+    # 4
+    M = deterministic_select(m, 0, len(m)-1, (len(m)+1)//2)
+    print("M[%d]" % M)
+
+    q = partition(li, p, r, M)
+    print("q[%d]" % q)
+
+    k = q-p+1
+
+    if i < k:
+        return deterministic_select(li, p, q-1, i)
+    elif i > k:
+        return deterministic_select(li, q+1, r, i-k)
+    else:
+        return li[q]
 
 if __name__ == '__main__':
     dir = "./test1/"
@@ -101,8 +141,6 @@ if __name__ == '__main__':
     t_end = time.time()
 
     with open(dir+"deter.txt", "w") as f_deter_out:
-        f_deter_out.write(el)
-        f_deter_out.write("\n")
-        f_random_out.write(t_end - t_start)
+        f_deter_out.write("{}\n{}".format(el, t_end - t_start))
 
     # check random_select
