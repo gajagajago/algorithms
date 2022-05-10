34d5156864d15c84e95e8b90a8205fabda434271
ryu <gajagajago@naver.com>
2022-05-10 17:08:33 +0900

    submit version

--- hw2/main.py
+++ hw2/main.py
@@ -23,56 +23,6 @@ class RBNode:
         if not self.isNil():
             self.size = self.lc.size + self.rc.size + 1
 
-    def display(self):
-        lines, *_ = self.__display_aux()
-        for line in lines:
-            print(line)
-
-    def __display_aux(self):
-        """Returns list of strings, width, height, and horizontal coordinate of the root."""
-        # No child.
-        if self.rc is None and self.lc is None:
-            line = '{}({},{})'.format(self.val, self.size, self.color)
-            width = len(line)
-            height = 1
-            middle = width // 2
-            return [line], width, height, middle
-
-        # Only left child.
-        if self.rc is None:
-            lines, n, p, x = self.lc.__display_aux()
-            s = '{}({},{})'.format(self.val, self.size, self.color)
-            u = len(s)
-            first_line = (x + 1) * ' ' + (n - x - 1) * '_' + s
-            second_line = x * ' ' + '/' + (n - x - 1 + u) * ' '
-            shifted_lines = [line + u * ' ' for line in lines]
-            return [first_line, second_line] + shifted_lines, n + u, p + 2, n + u // 2
-
-        # Only right child.
-        if self.lc is None:
-            lines, n, p, x = self.rc.__display_aux()
-            s = '{}({},{})'.format(self.val, self.size, self.color)
-            u = len(s)
-            first_line = s + x * '_' + (n - x) * ' '
-            second_line = (u + x) * ' ' + '\\' + (n - x - 1) * ' '
-            shifted_lines = [u * ' ' + line for line in lines]
-            return [first_line, second_line] + shifted_lines, n + u, p + 2, u // 2
-
-        # Two children.
-        left, n, p, x = self.lc.__display_aux()
-        right, m, q, y = self.rc.__display_aux()
-        s = '{}({},{})'.format(self.val, self.size, self.color)
-        u = len(s)
-        first_line = (x + 1) * ' ' + (n - x - 1) * '_' + s + y * '_' + (m - y) * ' '
-        second_line = x * ' ' + '/' + (n - x - 1 + u + y) * ' ' + '\\' + (m - y - 1) * ' '
-        if p < q:
-            left += [n * ' '] * (q - p)
-        elif q < p:
-            right += [m * ' '] * (p - q)
-        zipped_lines = zip(left, right)
-        lines = [first_line, second_line] + [a + u * ' ' + b for a, b in zipped_lines]
-        return lines, n + m + u, max(p, q) + 2, n + u // 2
-
 
 class RBTree:
     def __init__(self):
@@ -428,7 +378,7 @@ class RBTree:
 
 if __name__ == '__main__':
     # Fill in the directory where input.txt is located
-    dir = "./input0/"
+    dir = "./input1/"
     line_cnt = 0
 
     with open(dir + "output.txt", "w") as f_out:
@@ -463,6 +413,7 @@ if __name__ == '__main__':
     ## checker
     MAX = 9999
     A = [0] * (MAX + 1)
+
     finalCheck = True
 
     with open(dir + "checker.txt", "w") as f_check:
